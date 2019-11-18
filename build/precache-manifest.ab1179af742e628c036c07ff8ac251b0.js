444e6c0a7ab905eb9b7971b8c87714bb27e1dd88
Ani Karapetyan <anikarapetyan@Anis-MBP.fritz.box>
2019-11-19 00:45:47 +0100

    add build

---
+++ build/precache-manifest.ab1179af742e628c036c07ff8ac251b0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3653274d4bc8a125eb97ee69649dbac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "476a502d9e574a76a75f",
+    "url": "/static/css/2.18585a14.chunk.css"
+  },
+  {
+    "revision": "f2c225e04e62ecd23dcf",
+    "url": "/static/css/main.52996eed.chunk.css"
+  },
+  {
+    "revision": "476a502d9e574a76a75f",
+    "url": "/static/js/2.1f0545c7.chunk.js"
+  },
+  {
+    "revision": "f2c225e04e62ecd23dcf",
+    "url": "/static/js/main.a4caf3aa.chunk.js"
+  },
+  {
+    "revision": "84eabb17b451f3b1dc44",
+    "url": "/static/js/runtime-main.2b93b57f.js"
+  },
+  {
+    "revision": "33c8c03c1ce74163e981f12aaf003df6",
+    "url": "/static/media/bg-hero-gallery@2x.33c8c03c.jpg"
+  },
+  {
+    "revision": "502cb8e75fff78637dca6045efd5b4fc",
+    "url": "/static/media/bg-hero-home@2x.502cb8e7.jpg"
+  },
+  {
+    "revision": "f5caf6b84697041330912476217a1859",
+    "url": "/static/media/bg-menu@2x.f5caf6b8.jpg"
+  },
+  {
+    "revision": "0c704dfd416c85369ec8f3fd8e2d881a",
+    "url": "/static/media/bio-1@2x.0c704dfd.jpg"
+  },
+  {
+    "revision": "8b5e2dacc4335b97a514123ab7b83ad2",
+    "url": "/static/media/bio-2@2x.8b5e2dac.jpg"
+  },
+  {
+    "revision": "2d16ba993509f9b19c58f457933e226e",
+    "url": "/static/media/bio-3@2x.2d16ba99.jpg"
+  },
+  {
+    "revision": "58728c4235569fec02f952a970aa3496",
+    "url": "/static/media/bio-4@2x.58728c42.jpg"
+  },
+  {
+    "revision": "95b3ac3e072238c237e151b88070542e",
+    "url": "/static/media/bio-5@2x.95b3ac3e.jpg"
+  },
+  {
+    "revision": "9e82d58dcb65d1c6f8e3095470fd3ccb",
+    "url": "/static/media/bio-6@2x.9e82d58d.jpg"
+  },
+  {
+    "revision": "959e2995d1239079d35025ea5d23fc18",
+    "url": "/static/media/fonticons.959e2995.woff"
+  },
+  {
+    "revision": "e34e18c231a1bfd9ef10d7dfb7094e37",
+    "url": "/static/media/fonticons.e34e18c2.woff2"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
